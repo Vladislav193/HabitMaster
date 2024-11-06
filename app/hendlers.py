52d2c78813ea75c7013ca73b625623da130604a7
Vladislav <vladrosenrol@gmail.com>
2024-11-06 22:39:22 +0300

    прописываю hendlers

--- app/hendlers.py
+++ app/hendlers.py
@@ -1,3 +1,12 @@
 from aiogram import Router
-
+from aiogram.filters import CommandStart
+from aiogram.types import Message
 router= Router()
+
+
+@router.message(CommandStart())
+async def cmd_start(message: Message):
+    await message.answer("Привет! Я бот для остлеживания твоих привычек.")
+
+    user_id = message.from_user.id
+    user_name = message.from_user.first_name
